e7ace5326d93d6fab9e64dacda0ba554e37b3711
jaclealmena <jac.leal@duocuc.cl>
2024-05-29 09:23:13 -0400

    diccionario del ejercicio

---
+++ ejemplo_git.py
@@ -0,0 +1,8 @@
+#Ejercicio diccionario de hoy: usuarios:
+dic = {
+    "identificador": 1,
+    "nombre":"Marriot",
+    "direccion": "Avenida puerto rico 1234",
+    "funcionando": True,
+    "Valor habitación": 100000
+}
